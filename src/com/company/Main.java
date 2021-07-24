c281e170df54a179706080c3053a1afaff26c9bc
buetowcj <cbuetow@my.wctc.edu>
2021-07-23 19:14:07 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // M = P * (r(1+r)^n)/((1+r)^n-1)
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        int principal;
+        float annualInterest;
+        float monthlyInterest;
+        byte years;
+        int numberOfPayments;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Principal (1K - 1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1K and 1M.");
+        }
+
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+        }
+
+        while (true) {
+            System.out.print("Period (Years): ");
+            years = scanner.nextByte();
+            if (years > 0 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30.");
+        }
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow((1+monthlyInterest),numberOfPayments))
+                / (Math.pow((1+monthlyInterest),numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.print("Mortgage: " + mortgageFormatted);
+    }
+}
